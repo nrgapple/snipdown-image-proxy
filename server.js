88d7bbaa2025f3fc1d17a71d5f75fa266a0c8030
nrgapple <champtoday@gmail.com>
2020-07-16 14:51:48 -0400

    init

---
+++ server.js
@@ -0,0 +1,10 @@
+var proxy = require("html2canvas-proxy")
+var express = require("express")
+
+var app = express()
+var port = process.env.PORT || 3000
+
+app.use("/", proxy())
+
+console.log("Server running on port", port)
+app.listen(port)
