d378cdbe330efe6a13d202b33a8f6fde7799075f
nrgapple <champtoday@gmail.com>
2020-07-16 15:08:03 -0400

    add cors

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ var express = require("express")
 
 var app = express()
 var port = process.env.PORT || 3000
+// Convenience for allowing CORS on routes - GET only
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "GET, OPTIONS")
+  res.header("Access-Control-Allow-Headers", "Content-Type")
+  next()
+})
 
 app.use("/", proxy())
 
